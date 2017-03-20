5811491575a69829fc039a51b11cb1cd86f11aab
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-03-20 19:57:49 +1100

    Making the test fail again. Testing slack notification

--- experiment-src/hello_test.go
+++ experiment-src/hello_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestGreet(t *testing.T) {
-	expected := "Hello, USER!"
+	expected := "Hello, USER! // making this test fail"
 
 	if actual := _Greet("USER"); actual != expected {
 		t.Fatalf("Expected \"%s\", but got \"%s\"", expected, actual)
