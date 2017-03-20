23449befa0c68c505b9c40c8597ea11ffb658cf4
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-03-20 19:59:43 +1100

    Revert "Making the test fail again. Testing slack notification"
    This reverts commit 5811491575a69829fc039a51b11cb1cd86f11aab.

--- experiment-src/hello_test.go
+++ experiment-src/hello_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestGreet(t *testing.T) {
-	expected := "Hello, USER! // making this test fail"
+	expected := "Hello, USER!"
 
 	if actual := _Greet("USER"); actual != expected {
 		t.Fatalf("Expected \"%s\", but got \"%s\"", expected, actual)
